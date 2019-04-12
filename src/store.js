a1d47398b0ebe26546aa0eda8b70b20be5cc007b
armandoltx <armando@armandoltx.com>
2019-04-12 11:31:14 +1000

    added Middleware

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 
 const reducer = (state, action) => {
   if (action.type === "ADD_TO_CART"){
@@ -17,7 +17,14 @@ const reducer = (state, action) => {
   }
 
   return state;
+};
+// ejemplo de Middleware
+const logger = store => next => action => {
+  console.log('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  return result;
 }
 
-export default createStore(reducer, { cart: [] });
+export default createStore(reducer, { cart: [] }, applyMiddleware(logger));
 // a createStore se le pasa el reducer y el estado inicial, q en este caso es un array vacio
